9e3ba9f77c23794a461265962d6aa544fbb3595a
SebastianVillagra <sebastianvilla16.com@gmail.com>
2023-10-26 06:43:15 -0300

    Se agrego el ejercicio 5 del tp

---
+++ js/ejercicio5.js
@@ -0,0 +1,4 @@
+const numero1 = parseInt(prompt("Por favor introduce el primer número:"));
+const numero2 = parseInt(prompt("Ahora, introduce el segundo número por favor:"));
+//const resultado = numero1 + numero2; --> costumbre
+document.write('La suma de los dos números es: ', numero1 + numero2);
